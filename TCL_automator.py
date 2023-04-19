d701deadde738c392ccfc02e102922675837def2
Taylor J Seitz <40071528+tjseitz@users.noreply.github.com>
2023-04-19 15:49:53 -0400

    Update TCL_automator.py

--- TCL_automator.py
+++ TCL_automator.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 option = webdriver.ChromeOptions()
-option.add_argument("-incognito")
+option.add_argument("-incognito") #open website in incognito window
 option.add_experimental_option("excludeSwitches", ['enable-automation']);
 
 browser = webdriver.Chrome(executable_path='/Users/taylorjade/chromedriver', options=option)
@@ -13,3 +13,5 @@ checkboxes = browser.find_element("id", "i30").click()
 #create and click submit button
 submitbutton = browser.find_element("xpath", "/html/body/div/div[2]/form/div[2]/div/div[3]/div/div[1]/div/span/span").click()
 
+#click button to start a new response
+newresponse = browser.find_element("xpath", "/html/body/div[1]/div[2]/div[1]/div/div[4]/a").click()
